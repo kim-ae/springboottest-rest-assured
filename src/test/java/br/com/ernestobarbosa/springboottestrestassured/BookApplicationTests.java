c71ee1674f4ee27e3c3a4b0acedfa5d69d1f627a
José Ernesto Barbosa <jose.barbosa@cwi.com.br>
2019-02-01 14:12:01 -0200

    unit test

--- src/test/java/br/com/ernestobarbosa/springboottestrestassured/BookApplicationTests.java
+++ src/test/java/br/com/ernestobarbosa/springboottestrestassured/BookApplicationTests.java
@@ -1,5 +1,6 @@
 package br.com.ernestobarbosa.springboottestrestassured;
 
+import br.com.ernestobarbosa.springboottestrestassured.entity.Book;
 import br.com.ernestobarbosa.springboottestrestassured.model.Availability;
 import br.com.ernestobarbosa.springboottestrestassured.service.AvailabilityService;
 import org.junit.Test;
@@ -17,7 +18,10 @@ import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -36,6 +40,9 @@ public class BookApplicationTests {
     private AvailabilityService service;
 
     @MockBean
+    private AvailabilityService mockService;
+
+    @MockBean
     private RestTemplate template;
 
     @Value("${availability-id}")
@@ -43,17 +50,40 @@ public class BookApplicationTests {
 
     @Test
     public void unitTestMockedTest() throws Exception {
+        //controller
         this.mvc.perform(get("/")).andExpect(status().isOk())
                 .andExpect(content().json("[]"));
+        verify(service, times(1));
     }
 
+    //vendo
     @Test
-    public void componentAvailabilityFalseTestMockedTest() {
+    public void unitMockedTest() throws Exception {
+        this.mvc.perform(put("/").content("{\"id\": 1, \"name\": \"Ze\", \"price\": 10.0}")).andExpect(status().isOk());
+
+        this.mvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(content().json("[{\"id\": 1, \"name\": \"Ze\", \"price\": 10.0}]"));
+
+        verify(service, times(1));
+    }
+
+
+    @Test
+    public void serviceAvailabilityFalseTestMockedTest() {
+        //mock
         Availability mockAvailability = new Availability("Teste", false);
         when(template.getForEntity(any(String.class), any(Class.class))).thenReturn(new ResponseEntity(mockAvailability, HttpStatus.OK));
 
-        Availability av = service.getAvailabilityById(bookId);
-
-        assertFalse(av.isAvailable());
+        //service
+        assertFalse(service.getAvailabilityById(bookId).isAvailable());
     }
+
+    @Test
+    public void serviceAvailabilityMockedTest() {
+        //mock service
+        when(mockService.getAvailabilityById(any(Long.class))).thenReturn(new Availability("Teste", false));
+
+        assertFalse(mockService.getAvailabilityById(bookId).isAvailable());
+    }
+
 }
