0a7853199743b2481938264f4b19857ebc419dd8
Samuel Lucas <samuelborgeslucas@gmail.com>
2019-02-02 18:27:47 -0200

    fix classe service e add infos no README

--- src/main/java/br/com/ernestobarbosa/springboottestrestassured/service/AvailabilityService.java
+++ src/main/java/br/com/ernestobarbosa/springboottestrestassured/service/AvailabilityService.java
@@ -12,10 +12,11 @@ public class AvailabilityService {
     @Value("${availability-url}")
     private String AVAILABILITY_URL;
 
-    @Autowired
+//    @Autowired
     RestTemplate restTemplate;
 
     public Availability getAvailabilityById(Long id) {
+        restTemplate = new RestTemplate();
         return restTemplate.getForEntity(AVAILABILITY_URL + id, Availability.class).getBody();
     }
 }
