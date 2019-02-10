b3ccdae681009d20834a8d57efdb516eb5f78362
Marcos Vinicius Calicy da Silva <marcoscalicy@cwi.com.br>
2019-02-10 15:16:55 -0200

    Organização de Imports e POM; Allure no .gitignore;

---
+++ src/it/java/br/com/ernestobarbosa/springboottestrestassured/it/test/BookApplicationIntegrationTests.java
@@ -0,0 +1,55 @@
+import br.com.ernestobarbosa.springboottestrestassured.entity.Book;
+import com.google.gson.Gson;
+import core.BaseTest;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+
+public class BookApplicationIntegrationTests extends BaseTest {
+
+    @Test
+    public void noBooksTestIT() {
+        given()
+            .contentType(ContentType.JSON)
+        .when()
+            .get("/books/")
+        .then()
+            .statusCode(200)
+            .body("$", empty());
+    }
+
+    @Test
+    public void noAvailabilityTestIT() {
+        given()
+            .contentType(ContentType.JSON)
+            .pathParam("id", 100)
+        .when()
+            .get("/books/{id}/availability")
+        .then()
+            .statusCode(404);
+    }
+
+    @Test
+    public void createBooksTestIT() {
+        Book book = new Book().builder().name("My Book").price(10.0).build();
+        given()
+            .contentType(ContentType.JSON)
+            .body(new Gson().toJson(book))
+        .when()
+            .post("/books/")
+        .then()
+            .statusCode(201);
+
+        given()
+            .contentType(ContentType.JSON)
+        .when()
+            .get("/books/")
+        .then()
+            .statusCode(200)
+        .body("$", hasSize(greaterThan(0)));
+    }
+
+}
